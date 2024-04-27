14b74fcc639321f6bfc76fa884c40afc9863afca
pablo perez <perez51160900@hotmail.com>
2024-04-27 23:38:24 +0200

    creating database configuration

--- src/utils/env.validation.ts
+++ src/utils/env.validation.ts
@@ -3,4 +3,5 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
   PORT: Joi.string().default('3000'),
   ENV: Joi.string().default('DEV'),
+  DB_URL: Joi.string(),
 });
