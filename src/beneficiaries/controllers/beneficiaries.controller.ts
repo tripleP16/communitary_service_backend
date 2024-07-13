d5b401b2360364545b781f757e8a1f7c06337687
pablo perez <perez51160900@hotmail.com>
2024-07-13 13:10:47 +0200

    add medical history notification

--- src/beneficiaries/controllers/beneficiaries.controller.ts
+++ src/beneficiaries/controllers/beneficiaries.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Get,
   Param,
+  Patch,
   Post,
   Query,
   UseGuards,
@@ -14,6 +15,7 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { PaginationParamsDto } from 'src/utils/shared/dtos/pagination.params.dto';
 import { CreateBenficiariesDto } from '../dtos/create.benficiary.dto';
 import { BeneficiariesService } from '../services/benifciaries.service';
+import { CreateMedicalHistoryDto } from '../dtos/create.medical.history.dto';
 
 @ApiBearerAuth()
 @ApiTags('Beneficiaries')
@@ -33,6 +35,15 @@ export class BeneficiariesController {
   async getBeneficiaries(@Query() query: PaginationParamsDto) {
     return this.service.getBeneficiaries(query);
   }
+
+  @Actions('UPDATE_BENEFICIARY')
+  @UseGuards(AccessTokenGuard)
+  @UseGuards(RolesGuard)
+  @Patch('/medical/history/:id')
+  async addMedicalHistory(@Param('id') id: string, @Body() dto: CreateMedicalHistoryDto){
+    return this.service.addMedicalHistory(id, dto);
+  }
+
 
   @UseGuards(AccessTokenGuard)
   @Get('/:id')
