6c5b68c5ae5f72d7f256b40c2e75eb5d189345db
pablo perez <perez51160900@hotmail.com>
2024-08-13 19:15:30 +0200

    hotfix

--- src/privileges/mappers/privileges.mapper.ts
+++ src/privileges/mappers/privileges.mapper.ts
@@ -4,7 +4,7 @@ import { Privileges } from '../entities/privileges.entity';
 export class PrivilegesMapper {
   static mapToDao(privilege: Privileges): GetPrivilegesDao {
     return {
-      _id: privilege._id,
+      id: privilege._id,
       name: privilege.name,
     };
   }
